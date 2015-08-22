2c3cfce3e8ed7c1abddb9db7377ea98e0e70e896
Ryan Buckley <arebuckley@gmail.com>
2015-08-22 16:03:05 -0700

    fix bug in scraping it

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,6 +1,6 @@
 class BusinessesController < ApplicationController
   def create
-    if params[:business][:url]
+    if params[:business][:url].present?
       YelpJob.perform_later(params[:business][:url])
     end
     redirect_to root_path
